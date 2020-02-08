5f77f5469e6d801cbeecf5b81024689d5f8601e6
duraiarumugam <60323484+duraiarumugam@users.noreply.github.com>
2020-02-08 06:51:17 -0500

    UpdatemySQL
    Program to read from DB and update.

---
+++ UpdatemySQL.py
@@ -0,0 +1,18 @@
+#update employee table
+
+import pymysql
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="Enterprise"
+)
+cur = mydb.cursor()
+
+#cur.execute("update employee set empid=7 where empname='Krishna'")
+#mydb.commit()
+cur.execute("delete from employee where empid=7")
+mydb.commit()
+mydb.close()
